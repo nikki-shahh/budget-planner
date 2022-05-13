6040f7934a867fdc439da6830617a8b38aca7249
nikki-shahh <84827772+nikki-shahh@users.noreply.github.com>
2022-05-12 20:58:35 -0700

    add components and views

--- budget-planner/src/App.js
+++ budget-planner/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Budget Planner
         </p>
