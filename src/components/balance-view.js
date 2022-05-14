81ab923174317d4ced53277b27c0d8c242f50813
nikki-shahh <84827772+nikki-shahh@users.noreply.github.com>
2022-05-13 22:27:16 -0700

    adds balance - hard coded

---
+++ src/components/balance-view.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Balance() {
+    return (
+        <div className="alert alert-secondary">
+            <span>Balance <p>$ 1500</p></span>
+        </div>
+    );
+}
+
+export default Balance;
