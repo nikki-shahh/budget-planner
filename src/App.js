04173ef8f504832bb24284f80f303796113dffc8
nikki-shahh <84827772+nikki-shahh@users.noreply.github.com>
2022-05-13 22:58:52 -0700

    adds income- hard coded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Balance from './components/balance-view';
 import Expenses from './components/expense-view/expenses-total';
+import Income from './components/income-view/income-total';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -12,17 +13,19 @@ function App() {
         <h2 className="col-sm d-flex justify-content-end">Welcome</h2>
       </span>
       <div>
-        <div className="row mt-5">
-          <div className="col d-flex justify-content-center">
+        <div className="row mt-5 d-flex justify-content-center">
+          <div className="col-lg-6">
             <Balance />
           </div>
         </div>
-        <div className="row mt-2">
-          <div className="col-sm d-flex justify-content-center">
+        <div className="row mt-2 d-flex justify-content-center">
+          <div className="col-lg-2">
             <Expenses />
           </div>
-          <div className="col-sm d-flex justify-content-center">
-
+          <div className="col-lg-2">
+          </div>
+          <div className="col-lg-2">
+            <Income />
           </div>
         </div>
       </div>
---
+++ src/components/income-view/income-total.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Income() {
+    return (
+        <div className="alert alert-secondary">
+            <span>Income <p>$ 3500</p></span>
+        </div>
+    );
+}
+
+export default Income;
