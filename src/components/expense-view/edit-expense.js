82d460b98375abd6acc72c01e65645d6e6c0537b
nikki-shahh <84827772+nikki-shahh@users.noreply.github.com>
2022-05-18 20:39:13 -0700

    applies styles and adds logo

--- src/components/expense-view/edit-expense.js
+++ src/components/expense-view/edit-expense.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-function EditExpense(props) {
-    const [value, setValue] = useState(props.expense);
-    return (
-        <>
-            <input
-                required='required'
-                type='number'
-                className='form-control mr-3'
-                id='name'
-                value={value}
-                onChange={(event) => setValue(event.target.value)}
-            />
-            <button
-                type='button'
-                className='btn'
-                onClick={() => props.handleSaveClick(value)}
-            >
-                Save
-            </button>
-        </>
-    );
-};
+// function EditExpense(props) {
+//     const [value, setValue] = useState(props.expenses);
+//     return (
+//         <>
+//             <input
+//                 required='required'
+//                 type='number'
+//                 className='form-control mr-3'
+//                 id='name'
+//                 value={value}
+//                 onChange={(event) => setValue(event.target.value)}
+//             />
+//             <button
+//                 type='button'
+//                 className='btn'
+//                 onClick={() => props.handleSaveClick(value)}
+//             >
+//                 Save
+//             </button>
+//         </>
+//     );
+// };
 
-export default EditExpense;
+// export default EditExpense;
